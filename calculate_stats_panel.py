a90a868a3260f189816aeff52561cd44c3d82c18
Angela Yu <angelayu88@gmail.com>
2019-04-06 09:54:13 -0500

    in progress of debugging

--- calculate_stats_panel.py
+++ calculate_stats_panel.py
@@ -96,10 +96,8 @@ def R2D2_process(input_prefix, R2D2_output_dir, draw_dir, react_rhos, crystals_c
     constrained_c = 3.5
     cap_rhos = True
     weight_paired = 0.8
-
     sampled_structs_count = defaultdict(int)
     sampled_structs = set()
-
     # Vanilla Sampling
     if lock is None:
         structs, structs_labels = SU.RNAstructure_sample(input_prefix, e, R2D2_output_dir, label="noshape", num_proc=1, wn_tag="_%s"%(rnum))
@@ -107,7 +105,6 @@ def R2D2_process(input_prefix, R2D2_output_dir, draw_dir, react_rhos, crystals_c
         structs, structs_labels = SU.RNAstructure_sample(input_prefix, e, R2D2_output_dir, label="noshape", num_proc=1, wn_tag="_%s"%(rnum), lock=lock)
     sampled_structs.update(structs_labels)
     OSU.increment_dict_counts(sampled_structs_count, structs)
-
     # Sampling with SHAPE constraints
     if lock is None:
         structs, structs_labels = SU.RNAstructure_sample(input_prefix, e, R2D2_output_dir, shapefile=input_prefix+".rho", label="shape", num_proc=1, wn_tag="_%s"%(rnum))
@@ -115,7 +112,6 @@ def R2D2_process(input_prefix, R2D2_output_dir, draw_dir, react_rhos, crystals_c
         structs, structs_labels = SU.RNAstructure_sample(input_prefix, e, R2D2_output_dir, shapefile=input_prefix+".rho", label="shape", num_proc=1, wn_tag="_%s"%(rnum), lock=lock)
     sampled_structs.update(structs_labels)
     OSU.increment_dict_counts(sampled_structs_count, structs)
-
     # Sampling with hard constraints
     XB = SU.get_indices_rho_gt_c(react_rhos, constrained_c, one_index=True)  # RNAstructure is 1-indexed
     SU.make_constraint_file(output_prefix+".con", [], XB, [], [], [], [], [], [])
@@ -125,7 +121,6 @@ def R2D2_process(input_prefix, R2D2_output_dir, draw_dir, react_rhos, crystals_c
         structs, structs_labels = SU.RNAstructure_sample(input_prefix, e, R2D2_output_dir, constraintfile=output_prefix+".con", label="constrained_"+str(constrained_c), num_proc=1, wn_tag="_%s"%(rnum), lock=lock)
     sampled_structs.update(structs_labels)
     OSU.increment_dict_counts(sampled_structs_count, structs)
-
     # Compressing sampled structures further by removing duplicates sampled by multiple methods. Keeping track of this though.
     # Saving more than I need to in this use case... ex. energies
     sampled_structs = SU.merge_labels(sampled_structs, to_string=False)
@@ -133,14 +128,12 @@ def R2D2_process(input_prefix, R2D2_output_dir, draw_dir, react_rhos, crystals_c
     SU.cts_to_file(structs, crystals_ck[1], output_prefix+"_unique.ct")
     SU.runRNAstructure_efn2(output_prefix+"_unique.ct", output_prefix + ".efn2")
     free_energies = SU.get_free_energy_efn2(output_prefix + ".efn2")
-
     if cap_rhos:
         scaled_rhos = scaling_fns[scaling_func](react_rhos, scale_rho_max)
     else:
         scaled_rhos = scaling_fns[scaling_func](react_rhos)
     with open(input_prefix+".best_scaled_rho", "w") as f:
         f.write("\n".join(["\t".join([str(zi), str(zr)]) for zi,zr in enumerate(scaled_rhos)]))
-
     # Compute distances between scaled rhos and paired-vectors from drawn structures
     binary_structs = SU.ct_struct_to_binary_vec(structs)
     distances = []
@@ -148,7 +141,6 @@ def R2D2_process(input_prefix, R2D2_output_dir, draw_dir, react_rhos, crystals_c
         distances.append(SU.calc_bp_distance_vector_weighted(s, scaled_rhos, scaling_func=scaling_func, invert_struct="D" != scaling_func, paired_weight=weight_paired))
     min_distance = min(distances)
     min_dist_indices = [i for i, v in enumerate(distances) if v == min_distance]
-
     # compare R2D2 against crystal structure
     selected_react_mats = []
     for mdi in min_dist_indices:
@@ -162,7 +154,6 @@ def R2D2_process(input_prefix, R2D2_output_dir, draw_dir, react_rhos, crystals_c
         SU.ct_list_to_file(structs[mdi], crystals_ck[1], curr_prefix+".ct")
         SU.runRNAstructure_CircleCompare(curr_prefix+".ct", crystals_ck[3], curr_prefix+".ps")
         OSU.system_command("convert %s.ps %s.jpg" % (curr_prefix, curr_prefix))
-
     # saving R2D2 results
     R2D2_save = {}
     R2D2_save["structs"] = structs
@@ -172,7 +163,6 @@ def R2D2_process(input_prefix, R2D2_output_dir, draw_dir, react_rhos, crystals_c
     R2D2_save["scaled_rhos"] = scaled_rhos
     R2D2_save["react_mat"] = react_mat
     pickle.dump(R2D2_save, open(curr_prefix + ".p", "wb"))
-
     # output .dbn's like in normal R2D2 process
     # code taken from PCSU.generate_best_struct_images
     # PCSU.generate_best_struct_images contained some extraneuous calls for this use case. ex. draw_all = False, running VARNA
@@ -185,7 +175,6 @@ def R2D2_process(input_prefix, R2D2_output_dir, draw_dir, react_rhos, crystals_c
         if len(seen_snum) > 1:
             draw_outname_pre += "_mult" + str(sf)
         SU.run_ct2dot(output_prefix+"_unique.ct", seen_snum[sf], draw_outname_pre + ".dbn")
-
     # return curr_stats and selected structures
     return curr_stats, selected_react_mats
 
@@ -202,7 +191,7 @@ def parse_R2D2_process_output(out_stats, selected_react_mats, bm_R3D2_all, R2D2_
     return bm_R2D2_all, R2D2_all_selected, num_selected, R2D2_selected_binary_cts, R2D2_selected_binary_mats
 
 
-def write_R2D2_output_to_files(reactivities_prefix, R2D2_pairs, R2D2_consensus, react_rhos, crystals_mat, crystals_ct, cryst_seq):
+def write_R2D2_output_to_files(reactivities_prefix, R2D2_pairs, R2D2_consensus, R2D2_consensus_ct, react_rhos, crystals_mat, crystals_ctfile, crystals_ct, cryst_seq):
     # Write out results of R2D2 iterations
     with open("%s_R2D2_pairs.txt" % (reactivities_prefix), "w") as f:
         f.write("\n".join(["\t".join([str(bp) for bp in row]) for row in R2D2_pairs.tolist()]) + "\n")
@@ -212,6 +201,8 @@ def write_R2D2_output_to_files(reactivities_prefix, R2D2_pairs, R2D2_consensus, 
         f.write(str(SU.calc_benchmark_statistics_matrix(R2D2_consensus, crystals_mat)))
     write_reactivities_in_ct(SU.binary_mat_to_binary_ct(R2D2_consensus), react_rhos, reactivities_prefix+"_R2D2_consensus_ct_react.txt")
     SU.ct_list_to_file(R2D2_consensus_ct, cryst_seq, "%s_R2D2_consensus.ct" % (reactivities_prefix))
+    SU.runRNAstructure_CircleCompare("%s_R2D2_consensus.ct" % (reactivities_prefix), crystals_ctfile, "%s_R2D2_consensus.ps" % (reactivities_prefix))
+    OSU.system_command("convert %s_R2D2_consensus.ps %s_R2D2_consensus.jpg" % (reactivities_prefix, reactivities_prefix))
     write_reactivities_in_ct(crystals_ct, react_rhos, reactivities_prefix+"_crystal_ct_react.txt")
 
 
@@ -248,7 +239,6 @@ def run_Fold(seqfile, reactivities_prefix, react_rhos, num_proc, crystals_mat, c
         SU.runRNAstructure_fold(seqfile, "%s_%s.ct" % (reactivities_prefix, output_suffix), shapefile=reactivities_prefix+".rho", p=num_proc, shape_intercept=shape_intercept, shape_slope=shape_slope)
     else:
         SU.runRNAstructure_fold(seqfile, "%s_%s.ct" % (reactivities_prefix, output_suffix), p=num_proc, shape_intercept=shape_intercept, shape_slope=shape_slope)
-
     SU.runRNAstructure_CircleCompare("%s_%s.ct" % (reactivities_prefix, output_suffix), crystals_ctfile, "%s_%s.ps" % (reactivities_prefix, output_suffix))
     if lock is not None:
         lock.release()
@@ -279,7 +269,6 @@ def run_PCA(X, reactivities_prefix, center=False, scale_std=False):
     principal_components = pca.fit_transform(X)
     with open("%s_%sPCA_coords.txt" % (reactivities_prefix, output_prefix), "w") as f:
         f.write("\n".join(["\t".join([str(coord) for coord in row]) for row in principal_components.tolist()]) + "\n")
-
     return principal_components
 
 
@@ -320,6 +309,8 @@ def run_MDS_ct(X, reactivities_prefix, p=1):
     Runs a ct version of MDS
     X - assumed to be vectors of binary ct structures in each row
     """
+    if not isinstance(X, numpy.matrix):
+        X = numpy.matrix(X)
     distances = pairwise_distances(X, metric='manhattan', n_jobs=p)
     model = MDS(n_components=2, dissimilarity='precomputed', random_state=1)
     mds_coords = model.fit_transform(distances)
@@ -364,7 +355,6 @@ for k, v in reactivities.iteritems():
     reactivities[k][1] = react_seq[ind_of_match:end_of_match]
     SU.rhos_list_to_file(react_rhos, reactivities[k][2]+".rho")
     seqfile = NAU.make_seq(reactivities[k][1], reactivities[k][2]+".seq")
-
     # Folding - Fold with SHAPE, Fold with no SHAPE, 100 R2D2 iterations
     # R2D2 iteration done by taking code from cotranscriptional case (PCSU.run_cotrans_length) which has extraneous parts in this use case
     ## few lines in PCSU.run_cotrans_length made it unable to be used for this case. ex. length_key
@@ -380,8 +370,8 @@ for k, v in reactivities.iteritems():
         Fold_results = list(pool.imap(run_Fold_process_wrapper, args_pool))
         fold_shape_ct, fold_shape_react_mat = Fold_results[0]
         fold_noshape_ct, fold_noshape_react_mat = Fold_results[1]
-
-        args_pool = zip(repeat(reactivities[k][2]), repeat(R2D2_output_dir), repeat(draw_dir), repeat(react_rhos), repeat(crystals[ck]), range(100))
+        #args_pool = zip(repeat(reactivities[k][2]), repeat(R2D2_output_dir), repeat(draw_dir), repeat(react_rhos), repeat(crystals[ck]), range(1,101))
+        args_pool = zip(repeat(reactivities[k][2]), repeat(R2D2_output_dir), repeat(draw_dir), repeat(react_rhos), repeat(crystals[ck]), range(5))  # for debug
         for out_stats, selected_react_mats in pool.imap_unordered(R2D2_process_wrapper, args_pool):
             bm_R2D2_all, R2D2_all_selected, num_selected, R2D2_selected_binary_cts, R2D2_selected_binary_mats = parse_R2D2_process_output(out_stats, selected_react_mats, bm_R2D2_all, R2D2_all_selected, num_selected, R2D2_selected_binary_cts, R2D2_selected_binary_mats)
         del args_pool
@@ -391,23 +381,21 @@ for k, v in reactivities.iteritems():
         # no SHAPE folding
         fold_noshape_ct, fold_noshape_react_mat = run_Fold(seqfile, reactivities[k][2], react_rhos, num_proc, crystals[ck][2], crystals[ck][3], "Fold_noshape", shape_direct=False, shape_slope=shape_slope, shape_intercept=shape_intercept)
         # R2D2 iterations
-        for rnum in range(1,101):
-            out_stats, selected_react_mats = R2D2_process(input_prefix=reactivities[k][2], R2D2_output_dir=R2D2_output_dir, draw_dir=draw_dir, rnum=rnum)
+        #for rnum in range(1,101):
+        for rnum in range(1,5):  # for debug
+            out_stats, selected_react_mats = R2D2_process(reactivities[k][2], R2D2_output_dir, draw_dir, react_rhos, crystals[ck], rnum)
             bm_R2D2_all, R2D2_all_selected, num_selected, R2D2_selected_binary_cts, R2D2_selected_binary_mats = parse_R2D2_process_output(out_stats, selected_react_mats, bm_R2D2_all, R2D2_all_selected, num_selected, R2D2_selected_binary_cts, R2D2_selected_binary_mats)
-
     # finished R2D2 iterations for this dataset
     # report all and avg R2D2 results
     with open("%s_R2D2_all.stats" % (reactivities[k][2]), "w") as f:
         f.write(str(bm_R2D2_all) + "\n")
         for key in bm_R2D2_all:
             f.write("%s\t%s\n" % (key, sum(bm_R2D2_all[key]) / len(bm_R2D2_all[key])))
-
     # find basepairs that occurred over 50% of the time, then calculate accuracy
     R2D2_pairs = R2D2_all_selected / num_selected
     R2D2_consensus = R2D2_pairs.round()
     R2D2_consensus_ct = SU.binary_mat_to_ct(R2D2_consensus)
-    write_R2D2_output_to_files(reactivities[k][2], R2D2_pairs, R2D2_consensus, react_rhos, crystals[ck][2], crystals[ck][4], cryst_seq)
-
+    write_R2D2_output_to_files(reactivities[k][2], R2D2_pairs, R2D2_consensus, R2D2_consensus_ct, react_rhos, crystals[ck][2], crystals[ck][3], crystals[ck][4], cryst_seq)
     # PCA & MDS
     X = SU.ct_struct_to_binary_vec([fold_shape_ct, fold_noshape_ct, R2D2_consensus_ct, crystals[ck][4]])
     X += R2D2_selected_binary_cts
@@ -415,7 +403,6 @@ for k, v in reactivities.iteritems():
     Y += ["R2D2_iteration"] * len(R2D2_selected_binary_cts)
     colors = ['r', 'g', 'c', 'm'] + ['w'] * len(R2D2_selected_binary_cts)
     X_mats = [fold_shape_react_mat, fold_noshape_react_mat, R2D2_consensus, crystals[ck][2]] + R2D2_selected_binary_mats
-
     args_pool = [(run_PCA, (X, reactivities[k][2], False, False)),
                 (run_PCA, (X, reactivities[k][2], True, False)),
                 (run_PCA, (X, reactivities[k][2], False, True)),
@@ -427,7 +414,6 @@ for k, v in reactivities.iteritems():
     else:
         coords_list = [PCSU.calculate_function_helper(args_pool[i]) for i in range(len(args_pool))]
     del X, X_mats
-
     # plotting is not thread-safe, so do one by one
     plot_PCA(coords_list[0], Y, colors, reactivities[k][2], False, False)
     plot_PCA(coords_list[1], Y, colors, reactivities[k][2], True, False)
@@ -435,4 +421,4 @@ for k, v in reactivities.iteritems():
     plot_PCA(coords_list[3], Y, colors, reactivities[k][2], True, True)
     plot_MDS(coords_list[4], Y, colors, reactivities[k][2], "mat")
     plot_MDS(coords_list[5], Y, colors, reactivities[k][2], "ct")
-
+    break  # for debug
